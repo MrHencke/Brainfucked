c78cbd7bde988f163664f0003fe671f37baf662c
MrHencke <henckedev@outlook.com>
2022-05-25 15:29:30 +0200

    Init commit

---
+++ bf.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using std::ifstream;
+using std::string;
+
+// forward declarations
+string getFileContents(string fileName);
+void interpret(string instructions);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Please input a file to be interpreted";
+        return 0;
+    }
+    string instructions = getFileContents(argv[1]);
+    interpret(instructions);
+    return 0;
+}
+
+void interpret(string instructions)
+{
+    char tape[30000];
+    bool ignoreOps = false;
+    int index = 0;
+    int loop = 0;
+    for (int i = 0; i < instructions.length(); i++)
+    {
+        if (ignoreOps)
+        {
+            if (instructions[i] == ']')
+            {
+                ignoreOps = false;
+                i++;
+            }
+            else
+                continue;
+        }
+
+        switch (instructions[i])
+        {
+        case '>':
+            index += 1;
+            break;
+        case '<':
+            index -= 1;
+            break;
+        case '+':
+            tape[index] += 1;
+            break;
+        case '-':
+            tape[index] -= 1;
+            break;
+        case '[':
+            if (tape[index] == 0)
+            {
+                ignoreOps = true;
+            }
+            else
+            {
+                loop = i;
+            }
+            break;
+        case ']':
+            i = loop - 1;
+            break;
+        case '.':
+            std::cout << tape[index];
+            break;
+        }
+    }
+}
+
+string getFileContents(string fileName)
+{
+    string instructions;
+    ifstream file;
+    file.open(fileName);
+    string line;
+    while (std::getline(file, line))
+    {
+        instructions += line;
+    }
+    return instructions;
+}
---
+++ bf_ptr.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using std::ifstream;
+using std::string;
+
+// forward declarations
+string getFileContents(string fileName);
+void interpret(string instructions);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Please input a file to be interpreted";
+        return 0;
+    }
+    
+    string instructions = getFileContents(argv[1]);
+    interpret(instructions);
+    return 0;
+}
+
+void interpret(string instructions)
+{
+    char tape[30000];
+    char *pt = tape;
+    char *ptt = &instructions[0];
+    char *pttr;
+    bool ignoreOps = false;
+    while (ptt != NULL)
+    {
+        if (ignoreOps)
+        {
+            if (*ptt == ']')
+            {
+                ignoreOps = 0;
+                ptt++;
+            }
+            else
+            {
+                ptt++;
+                continue;
+            }
+        }
+
+        switch (*ptt)
+        {
+        case '>':
+            pt++;
+            break;
+        case '<':
+            pt--;
+            break;
+        case '+':
+            (*pt)++;
+            break;
+        case '-':
+            (*pt)--;
+            break;
+        case '[':
+            if (*pt == 0)
+            {
+                ignoreOps = 1;
+            }
+            else
+            {
+                pttr = ptt;
+            }
+            break;
+        case ']':
+            ptt = pttr;
+            continue;
+        case '.':
+            std::cout << *pt;
+            break;
+        }
+        ptt++;
+    }
+    delete pt;
+    delete ptt;
+    delete pttr;
+}
+
+string getFileContents(string fileName)
+{
+    string instructions;
+    ifstream file;
+    file.open(fileName);
+    string line;
+    while (std::getline(file, line))
+    {
+        instructions += line;
+    }
+    return instructions;
+}
